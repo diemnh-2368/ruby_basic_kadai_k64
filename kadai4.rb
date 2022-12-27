11c8e4f1a3175632d7d8646235ac6c7265fb879a
Xuandat1809 <91858880+Xuandat1809@users.noreply.github.com>
2022-12-27 19:10:52 +0700

    Update kadai4.rb

--- kadai4.rb
+++ kadai4.rb
@@ -1,8 +1,17 @@
-puts"学生䛾人数を入力してください >"
-n = gets.to.i
-arrname = []
-n.times.each do
-puts"#{a}番目䛾学生䛾名前䛿何ですか?"
-puts"名前 > "
-a += 1
+print '学生数を設定してください > '
+num = gets.chomp.to_i
+
+list_students = []
+
+(1..num).each do |i|
+  puts "#{i}番目の学生の名前は何ですか?"
+  print '名前 > '
+  name = gets.chomp
+  list_students << name
 end
+
+puts "-------------名簿----------------"
+list_students.each_with_index do |student, index|
+  puts "+ #{index + 1}番目の学生䛿#{student}です"
+end
+puts "--------------------------------" 
