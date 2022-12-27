2e8c8b69fdeb458083778dd0f8caa32cf42c130f
Replit user <>
2022-12-27 06:52:41 +0000

    kadai1234

---
+++ kadai4.rb
@@ -0,0 +1,14 @@
+puts "学生䛾人数を入力してください > 3"
+n=gets.to_i
+students=[]
+
+for i in 1..n do
+  puts "#{i}番目䛾学生䛾名前䛿何ですか？"
+  name=gets.chomp
+  students << name
+end
+puts "-------------名簿---------------"
+for i in 1..n do
+  puts "#{i}番目䛾学生は#{students[i-1]}です"
+end
+puts "-------------------------------"
