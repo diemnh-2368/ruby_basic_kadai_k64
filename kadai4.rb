49d07323fa4cc81b62ee39a639eab5bbf62db9d2
Replit user <>
2023-01-01 14:05:10 +0000

    kadai5

--- kadai4.rb
+++ kadai4.rb
@@ -1,21 +1,24 @@
 print "学生䛾人数を入力してください > "
-x = gets
+x = gets #「gets」メソッドを使ったら「enter」という特殊文字も扱う→改行してしまった
 names = []
 
 if x =~ /^[0-9]+$/
-  x = x.to_i
-for i in 0..x-1
-   puts "#{i+1}番目䛾学生䛾名前䛿何ですか?"
-   print "名前 > "
-   name = gets
-   names[i] = name
+  x = x.to_i #「^[0-9]+$」があったため、「to_i」が必要ない
+  for i in 0..x-1
+    puts "#{i+1}番目䛾学生䛾名前䛿何ですか?"
+   
+    print "名前 > "
+    name = gets
+    names[i] = name
+  end
+
+  puts "-------------名簿---------------"
+  for i in 0..x-1
+     puts "#{i+1}番目䛾学生は#{names[i]}です" 
+     #「{name[i].chomp}」を使わなければ、「\n」も扱う
+  end
+  puts "-------------------------------"
 end
 
-puts "-------------名簿---------------"
-for i in 0..x-1
-   puts "#{i+1}番目䛾学生は#{names[i]}です"
-end
-puts "-------------------------------"
-end
-
+# 参考：https://www.rubyguides.com/2019/10/ruby-chomp-gets/
 
